b184503a0962b5c84c720960286954acdb22202a
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-19 21:38:41 +0100

    Zmiana w zadaniu nr 7

--- Program.cs
+++ Program.cs
@@ -45,62 +45,6 @@ namespace sda_csharp_exercises
             Console.WriteLine(text);
             Console.WriteLine($"Po zmianie: {L33tCipher(text)}");
             Console.WriteLine($"Po zmianie: {L33tDecipher(text)}");
-=======
-
-        static long Strong(int number)
-        {
-            if (number < 2)
-                return 1;
-            else
-                return number * Strong(number - 1);
-        }
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine(checkPasswordComplexity("SLABE*HASL0")); //brak malej litery
-            Console.WriteLine(checkPasswordComplexity("slabe&hasl0")); //brak wielkiej litery
-            Console.WriteLine(checkPasswordComplexity("slabe*HASLO")); //brak cyfry
-            Console.WriteLine(checkPasswordComplexity("SLABEhasl0")); //brak znaku specjalnego
-            Console.WriteLine(checkPasswordComplexity("$lAb3")); //zbyt krotkie haslo
-            Console.WriteLine(checkPasswordComplexity("$ILNEhasl0")); //wszystkie warunki spełnione
-        }
-
-        /*
-        • długość co najmniej 6 znaków
-        • występują co najmniej jedna duża i jedna mała litera
-        • występuje co najmniej jedna cyfra
-        • występuje co najmniej jeden znak specjalny spośród następujących: &*$#
-        */
-        public static bool checkPasswordComplexity(string password)
-        {
-            Regex regexNum = new Regex(@"\d");
-            Regex regexLower = new Regex(@"[a-z]");
-            Regex regexUpper = new Regex(@"[A-Z]");
-            Regex regexSign = new Regex(@"[&*$#]");
-
-            if (!regexNum.IsMatch(password))
-                return false;
-
-            if (!regexLower.IsMatch(password))
-                return false;
-
-            if (!regexUpper.IsMatch(password))
-                return false;
-
-            if (!regexSign.IsMatch(password))
-                return false;
-
-            if (password.Length < 6)
-                return false;
-
-            return true;
-
-            Console.WriteLine("Podaj liczbę: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            long result = Strong(number);
-            Console.WriteLine(result);
-
-
         }
     }
 }
