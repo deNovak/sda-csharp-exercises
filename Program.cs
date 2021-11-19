12b5085d06b34ce1849cf382432f8b414c7cc22b
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-19 19:28:17 +0100

    Wykonanie zadania 4

--- Program.cs
+++ Program.cs
@@ -4,9 +4,34 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void PrintTriangle(int width, char character)
+        {
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine("");
+            }
+            for (int i = width - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine("");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Podaj szerokość podstawy: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj znak do budowy trójkąta: ");
+            char character = Convert.ToChar(Console.ReadLine());
+
+            PrintTriangle(width, character);
         }
     }
 }
