1e7cf70217617ad5486894fac4d5c5b3f61eaa36
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-19 20:50:10 +0100

    Wykonanie zadania 6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace sda_csharp_exercises
 {
@@ -6,7 +7,43 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(checkPasswordComplexity("SLABE*HASL0")); //brak malej litery
+            Console.WriteLine(checkPasswordComplexity("slabe&hasl0")); //brak wielkiej litery
+            Console.WriteLine(checkPasswordComplexity("slabe*HASLO")); //brak cyfry
+            Console.WriteLine(checkPasswordComplexity("SLABEhasl0")); //brak znaku specjalnego
+            Console.WriteLine(checkPasswordComplexity("$lAb3")); //zbyt krotkie haslo
+            Console.WriteLine(checkPasswordComplexity("$ILNEhasl0")); //wszystkie warunki spełnione
+        }
+
+        /*
+        • długość co najmniej 6 znaków
+        • występują co najmniej jedna duża i jedna mała litera
+        • występuje co najmniej jedna cyfra
+        • występuje co najmniej jeden znak specjalny spośród następujących: &*$#
+        */
+        public static bool checkPasswordComplexity(string password)
+        {
+            Regex regexNum = new Regex(@"\d");
+            Regex regexLower = new Regex(@"[a-z]");
+            Regex regexUpper = new Regex(@"[A-Z]");
+            Regex regexSign = new Regex(@"[&*$#]");
+
+            if (!regexNum.IsMatch(password))
+                return false;
+
+            if (!regexLower.IsMatch(password))
+                return false;
+
+            if (!regexUpper.IsMatch(password))
+                return false;
+
+            if (!regexSign.IsMatch(password))
+                return false;
+
+            if (password.Length < 6)
+                return false;
+
+            return true;
         }
     }
 }
