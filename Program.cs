79e1a850eaf76dc2c121903c58a722c6befa07b6
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-19 19:50:46 +0100

    Wykonanie zadania 5

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace sda_csharp_exercises
 {
+    //Napisz funkcję, która będzie wyliczała wartość funkcji silnia dla argumentu podanego przez użytkownika.
     class Program
     {
+        static long Strong(int number)
+        {
+            if (number < 2)
+                return 1;
+            else
+                return number * Strong(number - 1);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj liczbę: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            long result = Strong(number);
+            Console.WriteLine(result);
         }
     }
 }
