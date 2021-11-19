59bc3d864998484ca7032dc784c86ca4d306d406
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-19 20:55:57 +0100

    Wykonanie zadania 7

--- Program.cs
+++ Program.cs
@@ -4,9 +4,44 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static string L33tCipher(string text)
+        {
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] txt = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                txt[i] = text[i];
+                for (int j = 0; j < 5; j++)
+                {
+                    if (txt[i] == naturalLetters[j])
+                        txt[i] = leetLetters[j];
+                }
+            }
+            return string.Join("", txt);
+        }
+        static string L33tDecipher(string text)
+        {
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] txt = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                txt[i] = text[i];
+                for (int j = 0; j < 5; j++)
+                {
+                    if (txt[i] == leetLetters[j])
+                        txt[i] = naturalLetters[j];
+                }
+            }
+            return string.Join("", txt);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = "Stare dobre hakowanie";
+            Console.WriteLine(text);
+            Console.WriteLine($"Po zmianie: {L33tCipher(text)}");
+            Console.WriteLine($"Po zmianie: {L33tDecipher(text)}");
         }
     }
 }
