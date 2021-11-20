5f705b3e0ecd9d90c8ddc6fad23aa2fb778b9ced
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-20 13:11:31 +0100

    Wykonanie zadania 15

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Cat cat = new Cat("Mruczek");
@@ -11,6 +13,26 @@ namespace sda_csharp_exercises
             Dog dog = new Dog("Burek");
             dog.MakeSound();
 
+            List<Animal> animals = new List<Animal>();
+            animals.Add(cat);
+            animals.Add(dog);
+
+            foreach (Animal a in animals)
+            {
+                a.MakeSound();
+                if (a is Cat)
+                {
+                    Console.WriteLine("It's a cat");
+                }
+                else if (a is Dog)
+                {
+                    Console.WriteLine("It's a dog");
+                }
+                else
+                {
+                    Console.WriteLine("We don't know!");
+                }
+            }
         }
     }
 }
