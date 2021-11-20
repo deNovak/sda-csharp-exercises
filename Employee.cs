47df0a70b35b76d3c7dde3d7932500e2e4344b00
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-20 11:39:45 +0100

    Added constructor to Employye class

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Employee : Person 
+    {
+        public decimal Salary { get; set; }
+
+        public Employee(): this("", "", 1900, 0)
+            {
+             Console.WriteLine("Called Employee constructor without parameters");
+            }
+        public Employee(string firstName, string lastName, int yearOfBirth, decimal salary) :
+            base(firstName, lastName, yearOfBirth)
+        {
+            Console.WriteLine("Called Employee constructor with parameters");
+
+            Salary = salary;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Metoda 1//
+            //Person person = new Person();
+            //person.WhoAmI();
+            //person.FirstName = "Jan";
+            //person.LastName = "Kowalski";
+            //person.YearOfBirth = 1981;
+            //Console.WriteLine(person.IsAdult());
+
+            //Metoda 2//
+            //Person person = new Person()
+            //{
+            //    FirstName = "Jan",
+            //    LastName = "Kowalski",
+            //    YearOfBirth = 1981
+            //};
+            //person.WhoAmI();
+            //Console.WriteLine(person.IsAdult());
+
+            Employee employee = new Employee();
+            employee.FirstName = "Jan";
+            employee.LastName = "Kowalski";
+            employee.YearOfBirth = 1981;
+            employee.Salary = 5000;
+            employee.WhoAmI();
+            Employee employee2 = new Employee("Adam", "Nowak", 2000, 5000);
+            employee2.WhoAmI();
+
         }
     }
 }
