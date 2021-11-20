f07f7d10a50b08849d06f5b5f928638c27c87a8c
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-20 15:01:10 +0100

    Wykonanie zadania 17

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj E00 :-)!");
+            //Chleb, cena: 3.50 PLN, 5 szt, wartość: 17.50 PLN”
+            OrderItem item1 = new OrderItem("Bread", 5, 3.50M);
+            //Mleko, cena: 2.20 PLN, opakowanie: butelka,
+            //cena opakowania: 1 PLN, 5 szt, wartość: 16 PLN”
+            OrderItemWithPackage item2 = new OrderItemWithPackage("Milk", 5, 2.20M, "Bottle", 1M);
+
+            Order order = new Order();
+            order.AddOrderItem(item1);
+            order.AddOrderItem(item2);
+
+            order.DisplayOrderItems();
         }
     }
 }
