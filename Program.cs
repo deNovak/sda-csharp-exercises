83acb9a410ce105c28191a049848cfa612a1fb0f
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-20 12:03:53 +0100

    Type checking using GetType and typename

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
@@ -24,14 +25,49 @@ namespace sda_csharp_exercises
             //person.WhoAmI();
             //Console.WriteLine(person.IsAdult());
 
-            Employee employee = new Employee();
-            employee.FirstName = "Jan";
-            employee.LastName = "Kowalski";
-            employee.YearOfBirth = 1981;
-            employee.Salary = 5000;
-            employee.WhoAmI();
-            Employee employee2 = new Employee("Adam", "Nowak", 2000, 5000);
-            employee2.WhoAmI();
+            //Employee employee = new Employee();
+            //employee.FirstName = "Jan";
+            //employee.LastName = "Kowalski";
+            //employee.YearOfBirth = 1981;
+            //employee.Salary = 5000;
+            //employee.WhoAmI();
+            //Employee employee2 = new Employee("Adam", "Nowak", 2000, 5000);
+            //employee2.WhoAmI();
+
+            //Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
+            //Console.WriteLine(employee.Salary);
+            //Person person = new Employee("Adam", "Nowak", 2000, 6000);
+            ////Console.WriteLine(person.Salary);
+            //Employee employee2 = (Employee)person;
+            //Console.WriteLine(employee2.Salary);
+
+            Person person = new Person("Jan", "Kowalski", 1981);
+            Employee employee = new Employee("Adam", "Nowak", 2000, 5000);
+            Person person2 = new Employee("Joanna", "Kowalska", 1982, 6000);
+            List<Person> people = new List<Person>();
+            people.Add(person);
+            people.Add(employee);
+            people.Add(person2);
+
+            foreach (Person p in people)
+            {
+                if (p.GetType() == typeof(Person))
+                {
+                    p.WhoAmI();
+                    Console.WriteLine("Is Person");
+                }
+                else if (p.GetType() == typeof(Employee))
+                {
+                    p.WhoAmI();
+                    Console.WriteLine("Is Employee");
+                    Employee e = (Employee)p;
+                    Console.WriteLine($"Salary = {e.Salary}");
+                }
+                else
+                {
+                    Console.WriteLine("No idea !?");
+                }
+            }
 
         }
     }
