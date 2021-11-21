0a42e91e88a6e804650427b743b078314fa6606f
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-21 13:30:12 +0100

    Wykonanie zadania 23

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 namespace sda_csharp_exercises
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Witaj E00 :-)!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyPoint myPoint1 = new MyPoint(4, 7);
+			MyPoint myPoint2 = new MyPoint(6, 9);
+
+			LineSegment lineSegment1 = new LineSegment(myPoint1, myPoint2);
+			LineSegment lineSegment2 = new LineSegment(6, 9, 4, 7);
+
+			Console.WriteLine(lineSegment1.Lenght());
+
+			Console.WriteLine(lineSegment2.Lenght());
+		}
+	}
 }
