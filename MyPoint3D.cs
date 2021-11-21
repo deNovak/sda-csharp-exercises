7a06a51cf9e0fb42dd9fbcff76e3ac3ce8c96473
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-21 14:25:26 +0100

    Wykonanie zadanai 24

---
+++ MyPoint3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class MyPoint3D : MyPoint
+    {
+        public double Z { get; set; }
+
+        public MyPoint3D(double x, double y, double z) : base(x, y)
+        {
+            Z = z;
+        }
+
+        public override double GetDistanceFromPoint(MyPoint point)
+        {
+            MyPoint3D newPoint = (MyPoint3D)point;
+
+            return Math.Sqrt(Math.Pow(newPoint.X - X, 2) + Math.Pow(newPoint.Y - Y, 2) + Math.Pow(newPoint.Z - Z, 2));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,19 +2,25 @@
 
 namespace sda_csharp_exercises
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			MyPoint myPoint1 = new MyPoint(4, 7);
-			MyPoint myPoint2 = new MyPoint(6, 9);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LineSegment lineSegment1 = new LineSegment(2, 5, 5, 2);
 
-			LineSegment lineSegment1 = new LineSegment(myPoint1, myPoint2);
-			LineSegment lineSegment2 = new LineSegment(6, 9, 4, 7);
+            //MyPoint myPoint1 = new MyPoint(2, 5);
+            //MyPoint myPoint2 = new MyPoint(5, 2);
 
-			Console.WriteLine(lineSegment1.Lenght());
+            //LineSegment lineSegment2 = new LineSegment(myPoint1, myPoint2);
 
-			Console.WriteLine(lineSegment2.Lenght());
-		}
-	}
+            //Console.WriteLine($"Line 1: {lineSegment1.Length()}\nLine 2: {lineSegment2.Length()}");
+
+            MyPoint myPoint3D1 = new MyPoint3D(2, 5, 2);
+            MyPoint myPoint3D2 = new MyPoint3D(1, 2, 1);
+
+            LineSegment lineSegment3 = new LineSegment(myPoint3D1, myPoint3D2);
+
+            Console.WriteLine(lineSegment3.Length());
+        }
+    }
 }
