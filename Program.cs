9d1edac5e8f3318b0023efa948f30d599408cc4f
dnovak <93680960+devachii@users.noreply.github.com>
2021-11-21 10:33:35 +0100

    Wykonanie zadania 21

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj E00 :-)!");
+            Bank bank = new Bank();
+            BasicAccount account = new BasicAccount(AccountType.Account);
+            account.IncreaseBalance(2000M);
+            //account.IncreaseInterest(11M);
+            BasicAccount savingAccount = new BasicAccount(AccountType.SavingAccount);
+            savingAccount.IncreaseBalance(1000M);
+            savingAccount.IncreaseInterest(200M);
+            bank.AddAccount(account);
+            bank.AddAccount(savingAccount);
+            Console.WriteLine(bank.GetValue());
+
         }
     }
 }
